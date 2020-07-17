e5cfddbee3c9cba7c3bedc33f539ef9ad0602225
sean <15888897624@163.com>
2020-07-17 18:56:05 +0800

    权限处理

--- app/src/main/java/com/frizzle/frizzlepush/MainActivity.java
+++ app/src/main/java/com/frizzle/frizzlepush/MainActivity.java
@@ -35,6 +35,8 @@ public class MainActivity extends AppCompatActivity {
             }else {
                 livePusher.setPreviewDisplay(surfaceView.getHolder());
             }
+        } else {
+            livePusher.setPreviewDisplay(surfaceView.getHolder());
         }
     }
 
